08b2a61dd60d99eb09505ecbe26b14bc92c487c0
binbakhsh <mohsin.ba7866@gmail.com>
2025-05-12 20:07:58 +0400

    Update s3_toolkit.py

--- services/s3_toolkit.py
+++ services/s3_toolkit.py
@@ -1,45 +1,15 @@
-# Copyright (c) 2025 Stephen G. Pope
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-
-
-import os
-import boto3
-import logging
-from urllib.parse import urlparse, quote
-
-logger = logging.getLogger(__name__)
-
 def upload_to_s3(file_path, s3_url, access_key, secret_key, bucket_name, region):
-    # Parse the S3 URL into bucket, region, and endpoint
-    #bucket_name, region, endpoint_url = parse_s3_url(s3_url)
-    
     session = boto3.Session(
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_key,
         region_name=region
     )
-    
-  client = session.client(
-    's3',
-    endpoint_url=s3_url,
-    verify=os.environ.get("S3_VERIFY_SSL", "true").lower() == "true"
-)
 
-
+    client = session.client(
+        's3',
+        endpoint_url=s3_url,
+        verify=os.environ.get("S3_VERIFY_SSL", "true").lower() == "true"
+    )
 
     try:
         # Upload the file to the specified S3 bucket
